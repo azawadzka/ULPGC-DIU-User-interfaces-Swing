98dd6a1df7071949ea891cd8a01e27c6cd8a841c
Ania <azawadzka@vp.pl>
2019-11-12 14:54:20 +0100

    Add project

---
+++ Estadisticas subimagen/src/Canvas.java
@@ -0,0 +1,23 @@
+
+import java.awt.Graphics;
+import java.awt.event.AdjustmentEvent;
+import java.awt.event.AdjustmentListener;
+import javax.swing.JScrollBar;
+
+public class Canvas extends javax.swing.JScrollPane {
+
+
+    
+    
+    public Canvas() {
+        
+    }
+
+    @Override
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+    }
+    
+}
+
+
